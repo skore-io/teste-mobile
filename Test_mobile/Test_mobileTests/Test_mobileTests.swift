07598bf809f930f043350aff701b73f1f5f7a3f0
Rodrigo Miyashiro <rodrigo.miyashiro@homerefill.com.br>
2020-03-22 22:32:35 -0300

    Initial commit.

---
+++ Test_mobile/Test_mobileTests/Test_mobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_mobileTests.swift
+//  Test_mobileTests
+//
+//  Created by Rodrigo Miyashiro on 22/03/20.
+//  Copyright © 2020 Rodrigo Miyashiro. All rights reserved.
+//
+
+import XCTest
+@testable import Test_mobile
+
+class Test_mobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
