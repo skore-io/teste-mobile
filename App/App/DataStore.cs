e8213df6714c56eabe8257f342bb166c19d7a155
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-02 18:35:43 -0300

    Adicionar store genérica

---
+++ App/App/DataStore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace App
+{
+    public class DataStore<T> : IDataStore<T>
+    {
+        private readonly HashSet<T> _state = new HashSet<T>();
+        public IReadOnlyCollection<T> State
+        {
+            get => _state;
+        }
+
+        private T _selected = default;
+        public T Selected
+        {
+            get => _selected;
+        }
+
+        public void Add(T data) => _state.Add(data);
+        public void AddRange(IEnumerable<T> dataRange)
+        {
+            foreach (var data in dataRange)
+            {
+                Add(data);
+            }
+        }
+
+        public void Remove(T data) => _state.Remove(data);
+
+        public void Pick(T data) => _selected = data;
+
+        public void ClearState() => _state.Clear();
+        public void ClearSelected() => _selected = default;
+    }
+}
