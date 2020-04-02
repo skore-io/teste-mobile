af3e75694094e00597b65c02f06028ba2bda7fd4
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-02 19:26:31 -0300

    Adicionar carga da Store

--- App/App/ViewModels/MainPageViewModel.cs
+++ App/App/ViewModels/MainPageViewModel.cs
@@ -1,19 +1,52 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
+
+using App.Models;
+
+using Bogus;
+
+using Prism.Navigation;
 
 namespace App.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService)
+        private readonly Faker<Aula> _seeder;
+        private readonly IDataStore<Aula> _store;
+
+        public MainPageViewModel(Faker<Aula> seeder, IDataStore<Aula> store, INavigationService navigationService)
             : base(navigationService)
         {
-            Title = "Main Page";
+            Title = "Skore-IO Mobile";
+            _seeder = seeder ?? throw new ArgumentNullException(nameof(seeder));
+            _store = store ?? throw new ArgumentNullException(nameof(store));
+        }
+
+        private List<Aula> _aulas = new List<Aula>();
+        public List<Aula> Aulas
+        {
+            get { return _aulas; }
+            set { SetProperty(ref _aulas, value); }
+        }
+        
+        public override async void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            Refreshing = true;
+            // HACK: Simulando um pequeno delay para dar a impressão que estamos buscando algo na API
+            Func<Task<IEnumerable<Aula>>> fetchAndAwaitTask = async () =>
+            {
+                await Task.Delay(1000);
+                var result = _seeder.Generate(5);
+                await Task.Delay(500);
+                return result;
+            };
+            _store.AddRange(await fetchAndAwaitTask.Invoke());
+            Aulas = new List<Aula>(_store.State);
+            
+            Refreshing = false;
         }
     }
 }
