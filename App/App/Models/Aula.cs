4ad57083f43e48b1de0d3bbae8b0817ebac4cd44
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-02 18:38:26 -0300

    Adicionar Models Aula e Summary

---
+++ App/App/Models/Aula.cs
@@ -0,0 +1,17 @@
+﻿namespace App.Models
+{
+    public class Aula
+    {
+        public int CompanyId { get; set; }
+
+        public long CreatedAt { get; set; }
+
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string Status { get; set; }
+
+        public Summary Summary { get; set; } = null;
+    }
+}
---
+++ App/App/Models/Summary.cs
@@ -0,0 +1,7 @@
+﻿namespace App.Models
+{
+    public class Summary
+    {
+        public int? Percentage { get; set; }
+    }
+}
