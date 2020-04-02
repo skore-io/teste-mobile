42863a39c8a38adec654e0d711c6f0688d0e2615
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-02 19:03:28 -0300

    Adicionar gerador de aulas

---
+++ App/App/Models/AulaStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace App.Models
+{
+    /// <summary>
+    /// Possíveis status de uma aula.
+    /// </summary>
+    public static class AulaStatus
+    {
+        private const string _pendente = "NOT_STARTED";
+        private const string _iniciado = "IN_PROGRESS";
+        private const string _concluido = "COMPLETED";
+
+        public static IEnumerable<string> Status { get; } = new List<string>
+        {
+            _pendente,
+            _iniciado,
+            _concluido
+        };
+    }
+}
