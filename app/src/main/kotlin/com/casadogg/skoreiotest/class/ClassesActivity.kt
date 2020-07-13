94e3df51e56d4f1c508c9447e1577cad3ab76f49
Gabriel Casado <gdgcasado@gmail.com>
2020-07-13 15:17:45 -0300

    initial commit

---
+++ app/src/main/kotlin/com/casadogg/skoreiotest/class/ClassesActivity.kt
@@ -0,0 +1,12 @@
+package com.casadogg.skoreiotest.`class`
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.casadogg.skoreiotest.R
+
+class ClassesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_classes)
+    }
+}
