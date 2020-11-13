204c983f2fa36d54b4dc92823130fe9d026b4afa
Guilherme Oliveira <guioliveiraapps@gmail.com>
2020-11-13 13:03:15 -0300

    tratando onbackpressed e incluindo SOLUCAO.md

--- app/src/main/java/com/guioliveiraapps/skore/MainActivity.kt
+++ app/src/main/java/com/guioliveiraapps/skore/MainActivity.kt
@@ -2,11 +2,26 @@ package com.guioliveiraapps.skore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+        navController = findNavController(R.id.navController)
+
+    }
+
+    override fun onBackPressed() {
+        if (navController.previousBackStackEntry == null) {
+            moveTaskToBack(true)
+            return
+        }
+        super.onBackPressed()
     }
 }
