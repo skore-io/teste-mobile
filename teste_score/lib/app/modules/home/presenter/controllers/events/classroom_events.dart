ccdf9274adb9449a9b745580e70bf68249acbfcc
lucas3g <lucaslucassilva080397@gmail.com>
2022-08-27 13:45:59 -0300

    fix: events

--- teste_score/lib/app/modules/home/presenter/controllers/events/classroom_events.dart
+++ teste_score/lib/app/modules/home/presenter/controllers/events/classroom_events.dart
@@ -2,8 +2,6 @@ import 'package:teste_score/app/modules/home/domain/entities/classroom.dart';
 
 abstract class ClassRoomEvents {}
 
-class InitialEvent extends ClassRoomEvents {}
-
 class LoadingEvent extends ClassRoomEvents {}
 
 class SuccessGetListEvent extends ClassRoomEvents {
@@ -13,8 +11,6 @@ class SuccessGetListEvent extends ClassRoomEvents {
     required this.classRoom,
   });
 }
-
-class SuccessDeleteItemListEvent extends ClassRoomEvents {}
 
 class ErrorEvent extends ClassRoomEvents {
   final String message;
